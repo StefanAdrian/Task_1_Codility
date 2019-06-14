864ef82395e4b32d1459bf266ed1e08e978017ba
Adrian Berceanu <adrian.berceanu@protonmail.com>
2019-06-15 00:42:46 +0300

    tabs to spaces

--- Source.cpp
+++ Source.cpp
@@ -4,27 +4,27 @@ int solution(int A[], int N);
 
 int main()
 {
-	int arr[5] = { 1, 4, -1, 3, 2 };
-	int nodes = solution(arr, 5);
-	std::cout << "There are " << nodes << " nodes." << std::endl;
+    int arr[5] = { 1, 4, -1, 3, 2 };
+    int nodes = solution(arr, 5);
+    std::cout << "There are " << nodes << " nodes." << std::endl;
 }
 
 int solution(int A[], int N)
 {
-	int length = 1;
+    int length = 1;
 
-	for (int i = 0; i < N;)
-	{
-		if (A[i] == -1)
-		{
-			break;
-		}
-		else
-		{
-			i = A[i];
-			++length;
-		}
-	}
+    for (int i = 0; i < N;)
+    {
+        if (A[i] == -1)
+        {
+            break;
+        }
+        else
+        {
+            i = A[i];
+            ++length;
+        }
+    }
 
-	return length;
+    return length;
 }
